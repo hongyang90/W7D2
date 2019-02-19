7a5a9ec15d0578c2fc5485375d5097e4c894ee78
hongyang90 <hongyang.gao@gmail.com>
2019-02-18 18:21:52 -0800

    Add root.jsx and app.jsx and install Provider to grant global access to store

---
+++ todoApp/frontend/components/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App  = () => (
+  <h1>app app app </h1>
+);
+
+export default App;
--- todoApp/frontend/todo_redux.jsx
+++ todoApp/frontend/todo_redux.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', ()=> {
   const root = document.getElementById('root');
   const store = configureStore();
   window.store = store; //remove later
-  ReactDOM.render(<h1>Todos App</h1>, root);
+  ReactDOM.render(<Root store={ store } />, root);
 });
